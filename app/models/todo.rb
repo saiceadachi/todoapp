1f1acbc43fc2b1447a537a19d0695618d452ee2d
adachi <adachi@saice.net>
2023-07-12 11:31:04 +0900

    予定追加時にDiscordのWebHookに対してPOSTする機能を追加(DISCORD_WEBHOOK_URI環境変数にURIをセット)

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,15 @@
+require 'uri'
+require 'net/http'
+require 'json'
+ 
 class Todo < ApplicationRecord
+    after_create_commit :send_to_discord
+
+    def send_to_discord
+        if !(ENV['DISCORD_WEBHOOK_URI'] || '').empty? then
+            uri = URI(ENV['DISCORD_WEBHOOK_URI'])
+            str = JSON.generate({ "content" => "予定が追加されました\n"+self.title+"\n"+self.decription+"\n期限は"+self.deadline.to_json+"です", "username" => "Todo"})
+            res = Net::HTTP.post(uri, str, {"content-type"=>'application/json; charset=UTF-8'} )
+        end
+    end
 end
